99e28e0a5421a2e0d577d10de269363c3b30517d
plausibility <chris@gibsonsec.org>
2013-04-30 13:36:15 +1000

    PEP8 compliance in check.py

--- check.py
+++ check.py
@@ -2,8 +2,16 @@ import mongo
 from get_coin import difficulty
 from hashlib import sha512
 
-#{"cmd":"check", "winning_string":string, "winning_hash":hash, 'addr':addr}
+
 def check(obj, data):
+    """
+    {
+        "cmd": "check",
+        "winning_string": <string>,
+        "winning_hash": <hash_of_string>,
+        "addr": <address_to_reward>
+    }
+    """
     try:
         winstr = str(data['winning_string'])
         winhash = str(data['winning_hash'])
@@ -12,18 +20,20 @@ def check(obj, data):
         obj.send("False")
         obj.close()
         return
-    
-    if not mongo.db.addresses.find_one({"addr":addr}):
+
+    if not mongo.db.addresses.find_one({"addr": addr}):
         obj.send("False")
         obj.close()
         return
 
-    if winhash == sha512(winstr).hexdigest() and not mongo.db.coins.find_one({"hash":winhash}) and winhash.startswith(difficulty() * "0") and mongo.db.addresses.find_one({"addr":addr}):
+    is_same_hash = winhash == sha512(winstr).hexdigest()
+    coin_exists = mongo.db.coins.find_one({"hash": winhash})
+    correct_diff = winhash.startswith(difficulty() * "0")
+    address_exists = mongo.db.addresses.find_one({"addr": addr})
+    if is_same_hash and not coin_exists and correct_diff and address_exists:
         obj.send("True")
-        mongo.db.coins.insert({"hash":winhash, "addr":addr})
+        mongo.db.coins.insert({"hash": winhash, "addr": addr})
     else:
         obj.send("False")
     obj.close()
     return
-    
-    
