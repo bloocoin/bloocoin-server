d51142ebd69e721fea6990245953ec83b821d6dd
plausibility <chris@gibsonsec.org>
2013-04-30 13:24:35 +1000

    PEP8 compliance in init.

--- __init__.py
+++ __init__.py
@@ -11,14 +11,14 @@ import json
 
 
 cmds = {
+    "get_coin": get_coin.get_coin,
+    "register": register.register,
+    "send_coin": send_coin.send_coin,
+    "my_coins": my_coins.my_coins,
+    "check": check.check,
+    "transactions": transactions.transactions,
+}
 
-            "get_coin":get_coin.get_coin, #
-            "register":register.register,  #         #Commands of the BlooCoin protocol
-            "send_coin":send_coin.send_coin,
-            "my_coins":my_coins.my_coins, #
-            "check":check.check,
-            "transactions":transactions.transactions, #
-        }
 
 def main():
     port = 3122
@@ -31,20 +31,25 @@ def main():
         obj, conn = sock.accept()
         try:
             data = obj.recv(1024)
-        except:
-            obj.close()   #This will only happen if the client suddenly disconnects as it is making a request.
+        except IOError:
+            # This will only happen if the client suddenly
+            # disconnects as it is making a request.
+            obj.close()
             continue
         print conn[0], data
         if data:
-            threading.Thread(target=handle, args=(data, obj)).start() #If data we start some need to then parse the data in json format.
+            #If data we start some need to then parse the data in json format.
+            threading.Thread(target=handle, args=(data, obj)).start()
         else:
             continue
 
-def handle(data, obj): #Function for parsing commands, {'cmd':command}
+
+def handle(data, obj):  # Function for parsing commands, {'cmd':command}
     try:
         data = json.loads(data)
         cmds[data[u'cmd']](obj, data)
-    except Exception, error:                     #If data is not in the json format it will log the error.
+    except Exception as error:
+        # If data is not in the json format it will log the error.
         print error
         #with open("log.txt", 'a') as file:
             #file.write(error)
@@ -52,8 +57,3 @@ def handle(data, obj): #Function for parsing commands, {'cmd':command}
 
 if __name__ == "__main__":
     main()
-
-
-        
-
-
