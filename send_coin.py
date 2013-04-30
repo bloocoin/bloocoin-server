12a70f14e4c075dac0bb273ac747c0336081952d
plausibility <chris@gibsonsec.org>
2013-04-30 13:57:23 +1000

    PEP8 compliance in send_coin.py as well!

--- send_coin.py
+++ send_coin.py
@@ -1,7 +1,15 @@
 import mongo
 
+
 def send_coin(obj, data):
-    #{"cmd":'send_coin', 'amount':_, 'to':addr, 'addr':addr}
+    """
+    {
+        "cmd": "send_coin",
+        "amount": _,
+        "to": addr,
+        "addr": addr
+    }
+    """
     try:
         amount = int(data[u'amount'])
         to = str(data[u'to'])
@@ -11,22 +19,28 @@ def send_coin(obj, data):
         obj.send("Error")
         obj.close()
         return
-                    
-    if mongo.db.addresses.find_one({"addr":addr, "pwd":pwd}) and  mongo.db.addresses.find_one({"addr":addr}): #All conditions
-        if amount <= 0: #checking amounts
+
+    valid_account = mongo.db.addresses.find_one({"addr": addr, "pwd": pwd})
+    valid_recipient = mongo.db.addresses.find_one({"addr": to})
+    if valid_account and valid_recipient:
+        if amount <= 0:  # checking amounts
             obj.send("Amount can not be zero")
             obj.close()
             return
 
         coins = 0
-        for x in mongo.db.coins.find({"addr":addr}):
+        for x in mongo.db.coins.find({"addr": addr}):
             coins += 1
         if coins >= amount:
             for x in xrange(0, amount):
-                before = mongo.db.coins.find_one({"addr":addr})
+                before = mongo.db.coins.find_one({"addr": addr})
                 before['addr'] = to
-                mongo.db.coins.update({'addr':addr}, before)
-            mongo.db.transactions.insert({"to":to, "from":addr, "amount":amount})     
+                mongo.db.coins.update({"addr": addr}, before)
+            mongo.db.transactions.insert({
+                "to": to,
+                "from": addr,
+                "amount": amount
+            })
             obj.send("Transaction Successful!")
             obj.close()
             return
