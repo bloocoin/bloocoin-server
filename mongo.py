9580a29cb5d300431bf00b175038adf3203a52ac
Frankie <max00355@gmail.com>
2013-05-11 19:01:09 -0400

    Added support for remote authentication of Mongo, great for scaling

--- mongo.py
+++ mongo.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 import pymongo
 
-db = pymongo.MongoClient("localhost", 27017).bloocoin
+#Only change for remote authentication
+host = "localhost"
+port = 27017
+username = ""
+password = ""
+
+db = pymongo.MongoClient(host, port).bloocoin
+db.authenticate(username, password)
